9058744b03b7ac3d14e16496bd35912d2535a86b
Adroaldo <hygino@msn.com>
2022-07-17 10:27:27 -0300

    Post record

--- backend/src/main/java/com/devsuperior/dspesquisa/controllers/RecordController.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/controllers/RecordController.java
@@ -1,10 +1,27 @@
 package com.devsuperior.dspesquisa.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.services.RecordService;
 
 @RestController
 @RequestMapping("/records")
 public class RecordController {
 
+	@Autowired
+	public RecordService recordService;
+
+	@PostMapping
+	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto) {
+		RecordDTO recordDTO = recordService.insert(dto);
+
+		return ResponseEntity.ok().body(recordDTO);
+	}
 }
